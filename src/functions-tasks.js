867e7e6a6d038605230d564e8dc8058b8e24b496
Alexey888888 <brdkmail@gmail.com>
2024-03-21 12:11:34 +0200

    feat: implement retry function

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -137,8 +137,17 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return (arg) => {
+    for (let i = 0; i <= attempts; i += 1) {
+      try {
+        return func(arg);
+      } catch (error) {
+        if (i === attempts) throw new Error(error);
+      }
+    }
+    return '';
+  };
 }
 
 /**
