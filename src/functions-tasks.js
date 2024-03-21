95b4a296c9d44e0eab3799468b9b715970289c3c
Alexey888888 <brdkmail@gmail.com>
2024-03-21 20:52:02 +0200

    feat: implement getIdGeneratorFunction function

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -196,8 +196,10 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return (...args) => {
+    return fn(...args1, ...args);
+  };
 }
 
 /**
@@ -217,8 +219,13 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let id = startFrom;
+  return () => {
+    const resId = id;
+    id += 1;
+    return resId;
+  };
 }
 
 module.exports = {
